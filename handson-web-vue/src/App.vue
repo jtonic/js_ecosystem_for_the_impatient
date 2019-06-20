c3447275856c83099a53b45924a06bad35313a1c
Antonel-Ernest Pazargic <antonel-ernest.pazargic@ing.com>
2019-06-20 07:24:12 +0300

    new vue application with vue-cli generator

---
+++ handson-web-vue/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <hello-world msg="Welcome to Your from Vue.js App!!!"/>
+    <hello-again/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+import HelloAgain from "./components/HelloAgain.vue";
+
+export default {
+  name: "app",
+  components: {
+    HelloWorld,
+    HelloAgain
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 10px;
+}
+</style>
---
+++ handson-web-vue/src/components/HelloAgain.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <div>Hello for again from Vue world!!!</div>
+    <div>Your age is {{age}}</div>
+    <button v-on:click="incrementUp">Increment your age;)</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      age: 0
+    };
+  },
+  methods: {
+    incrementUp: function() {
+      this.age++;
+    }
+  }
+};
+</script>
+
+<style scoped>
+div {
+  font-size: 16px;
+  font-weight: bold;
+  text-align: center;
+  color: tomato;
+  margin-top: 10px;
+}
+</style>
