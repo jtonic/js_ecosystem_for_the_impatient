4a0dce00660d7ac1d0df602bf8f707eca9fe401b
Antonel-Ernest Pazargic <antonel.pazargic@gmail.com>
2019-06-28 21:28:12 +0300

    some changes

--- handson-vue-electron/my-todos/src/main/index.js
+++ handson-vue-electron/my-todos/src/main/index.js
@@ -7,23 +7,52 @@ import { app, BrowserWindow } from 'electron'
  * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
  */
 if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\')
+  global.__static = require('path')
+    .join(__dirname, '/static')
+    .replace(/\\/g, '\\\\')
 }
 
 let mainWindow
-const winURL = process.env.NODE_ENV === 'development'
-  ? `http://localhost:9080`
-  : `file://${__dirname}/index.html`
+const winURL =
+  process.env.NODE_ENV === 'development'
+    ? `http://localhost:9080`
+    : `file://${__dirname}/index.html`
 
 function createWindow () {
   /**
    * Initial window options
    */
   mainWindow = new BrowserWindow({
-    height: 563,
+    backgroundColor: 'blue',
+    darkTheme: true,
+    height: 600,
     useContentSize: true,
-    width: 1000
+    width: 1000,
+    webPreferences: {
+      nativeWindowOpen: true
+    }
   })
+
+  mainWindow.once('ready-to-show', () => {
+    mainWindow.show()
+  })
+
+  // mainWindow.webContents.on(
+  //   'new-window',
+  //   (event, url, frameName, disposition, options, additionalFeatures) => {
+  //     if (frameName === 'modal') {
+  //       // open window as modal
+  //       event.preventDefault()
+  //       Object.assign(options, {
+  //         modal: true,
+  //         parent: mainWindow,
+  //         width: 400,
+  //         height: 600
+  //       })
+  //       event.newGuest = new BrowserWindow(options)
+  //     }
+  //   }
+  // )
 
   mainWindow.loadURL(winURL)
 
