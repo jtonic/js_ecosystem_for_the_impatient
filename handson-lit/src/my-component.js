92ccb2af584750fcf573475bec61db10758ddaa4
Antonel-Ernest Pazargic <antonel.pazargic@gmail.com>
2019-07-26 15:17:51 +0300

    using properties

--- handson-lit/src/my-component.js
+++ handson-lit/src/my-component.js
@@ -1,10 +1,31 @@
 import { LitElement, html } from "lit-element";
 
 class MyComponent extends LitElement {
+  static get properties() {
+    return {
+      message: { type: String },
+      name: { type: String }
+    };
+  }
+
+  constructor() {
+    super();
+    this.message = "Hello";
+    this.name = "Jtonic";
+  }
+
   render() {
     return html`
-      <p>Hello world!!!</p>
+      <p>${this.message} ${this.name}!!!</p>
+      <p>Your name reversed is ${this.reversedName(this.name)}</p>
     `;
+  }
+
+  reversedName(name) {
+    return name
+      .split("")
+      .reverse()
+      .join("");
   }
 }
 
