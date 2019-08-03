7f12ef47da2f571c448b6db40d7eda13880ae1e9
jtonic <antonel.pazargic@gmail.com>
2019-08-03 19:31:28 +0300

    vue.js - how to access vue data properties using vue instance

--- playground/pl_vue/index.js
+++ playground/pl_vue/index.js
@@ -1,9 +1,19 @@
-new Vue({
+let vue = new Vue({
   el: "#app",
   data() {
     return {
       message: "Hello Vue.JS world!!!"
     };
   },
-  template: "<h2>{{ message }}</h2>"
+  template: "<h2>{{ message }}</h2>",
+  methods: {
+    changeMessage() {
+      this.data.message = "Hello Mr. Vue.JS";
+    }
+  }
 });
+
+console.log(`vue data.message: ${vue.message}`);
+setTimeout(() => {
+  vue.message = "Hello Mr. Vue.JS!!!";
+}, 5000);
