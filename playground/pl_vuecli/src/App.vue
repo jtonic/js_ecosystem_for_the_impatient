8aef628db5896ec1f8721378edf5a620b91c3e25
jtonic <antonel.pazargic@gmail.com>
2019-08-04 11:14:55 +0300

    vue: create an app with vuecli ui

---
+++ playground/pl_vuecli/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">{{ message }}</div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      message: "Welcome to Your Vue.js App"
+    };
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
