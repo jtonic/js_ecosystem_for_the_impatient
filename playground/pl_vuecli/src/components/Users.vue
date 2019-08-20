79cfd2326f92fa9a612e646fc3e6d2a1c43c3daf
Antonel-Ernest Pazargic <antonel.pazargic@gmail.com>
2019-08-20 08:25:36 +0200

    use directive v-text instead of mustache based template

--- playground/pl_vuecli/src/components/Users.vue
+++ playground/pl_vuecli/src/components/Users.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
     <h1>Users</h1>
-    <h2>User id: {{ $route.params.id }}</h2>
+    <h2 v-text="users"></h2>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Users',
+  computed: {
+    users() {
+      return `User id: ${this.$route.params.id}`
+    },
+  },
 }
 </script>
